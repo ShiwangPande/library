fd41d99df6cd5f5fb27d7db6e4bb0e8d96f6bfff
shiwang pandey <110737840+ShiwangPande@users.noreply.github.com>
2023-01-10 00:19:29 +0530

    done 2

--- src/components/cards/Card8.js
+++ src/components/cards/Card8.js
@@ -10,7 +10,7 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>hibbler</p>
+                <p className='cards__title'>we</p>
             </div>
             <div className='cards'>
                 <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
@@ -18,79 +18,7 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>rc </p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>rc</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>hrllo</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>rc</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>hi</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>how</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>are</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>you</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>bro</p>
-            </div>
-            <div className='cards'>
-                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
-                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
-                <div className='cards__stats'>
-                    {/* <span>{props.id}</span> */}
-                </div>
-                <p className='cards__title'>dont</p>
+                <p className='cards__title'>don't </p>
             </div>
             <div className='cards'>
                 <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
@@ -106,7 +34,7 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>pdf</p>
+                <p className='cards__title'>8th</p>
             </div>
             <div className='cards'>
                 <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
@@ -114,7 +42,7 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>take</p>
+                <p className='cards__title'>sem</p>
             </div>
             <div className='cards'>
                 <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
@@ -122,7 +50,7 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>my</p>
+                <p className='cards__title'>book</p>
             </div>
             <div className='cards'>
                 <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
@@ -130,7 +58,79 @@ function Card8(props1) {
                 <div className='cards__stats'>
                     {/* <span>{props.id}</span> */}
                 </div>
-                <p className='cards__title'>help</p>
+                <p className='cards__title'>Plz</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>share</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>if</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>you</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>can</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>hope</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>you</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>loved</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>the</p>
+            </div>
+            <div className='cards'>
+                <a href='https://drive.google.com/file/d/1lVBMAEIGhimQhEMLQ5cQtDjCwV8TosHB/view?usp=sharing' target="_blank">
+                    <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzN8fGxlYXJufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60" alt="" /> </a>
+                <div className='cards__stats'>
+                    {/* <span>{props.id}</span> */}
+                </div>
+                <p className='cards__title'>app.</p>
             </div>
         </>
     )
